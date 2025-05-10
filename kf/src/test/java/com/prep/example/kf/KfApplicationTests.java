cf5de5607e556b67fb1ad6954d0867b18d5d272f
Aseef Jamaludeen(UST <u10028@ust.com>
2025-05-10 22:27:45 +0530

    Initial commit

---
+++ kf/src/test/java/com/prep/example/kf/KfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.example.kf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
