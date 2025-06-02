62d9cefe64c077ba304aca1c44d551b0368c1dd6
Aseef Jamaludeen(UST <u10028@ust.com>
2025-06-02 14:07:16 +0530

    checkstyle and gha first commit

--- kf/src/main/java/com/prep/example/kf/KfApplication.java
+++ kf/src/main/java/com/prep/example/kf/KfApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KfApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KfApplication.class, args);
+    }
 
 }
